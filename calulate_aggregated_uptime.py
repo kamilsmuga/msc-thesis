46ad5849223375f99cb90f28b6813f7c38ac1a6c
kamilsmuga <smugakamil@gmail.com>
2015-01-21 00:01:54 +0000

    store last timestamp

--- calulate_aggregated_uptime.py
+++ calulate_aggregated_uptime.py
@@ -11,20 +11,24 @@ machines = {}
 # total number of machines
 machines_count = 0
 
+# last timestamp
+last_timestamp = 0
+
 with open(MACHINES_CSV, 'rb') as machines_csv:
     machine_reader = csv.reader(machines_csv, delimiter=',')
     for row in machine_reader:
         machine_id = row[1]
-        event = row[2]
+        event = int(row[2])
         timestamp = int(row[0])
-        if timestamp > 600000000:
-            print machine_id
-            print timestamp
+        if timestamp > last_timestamp:
+            last_timestamp = timestamp
         if not machine_id in machines:
             machines[machine_id] = []
-        machines[machine_id].append((timestamp, event))
+        machines[machine_id].append({timestamp: event})
 
 machines_count = len(machines)
+print 'Total count of machines: %s' % machines_count
+print 'Last time stamp: %s' % last_timestamp
 
 """
 fileobj = open('test', 'w')
