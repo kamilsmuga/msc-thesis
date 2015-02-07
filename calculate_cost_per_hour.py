40f33e507e0ff59d435c1adc5f1c9676f60ec9f4
Kamil Smuga <smugakamil@gmail.com>
2015-02-07 23:03:30 +0000

    map function for Spark transformation

--- calculate_cost_per_hour.py
+++ calculate_cost_per_hour.py
@@ -2,7 +2,70 @@
 
 import matplotlib.pyplot as plot
 import csv
+from pyspark import SparkContext, SparkConf
 
+"""
+-------------------
+SPARK CONFIGURATION
+-------------------
+
+--------------------------------------------------------
+Used only for standalone execution via bin/spark-submit
+--------------------------------------------------------
+conf = (SparkConf()
+        .setMaster("local")
+        .setAppName("Uptime per machine")
+        .set("spark.executor.memory", "10g")
+        .set("spark.local.dir", "/home/ks/workspace/data/spark-out"))
+sc = SparkContext(conf = conf)
+"""
+
+def calc_task_time(line):
+    """
+    Map function to calculate total time spent on task 
+    Returns tuple: (machine, task_time)
+
+    Schema for task usage
+
+        1,start time,INTEGER,YES
+        2,end time,INTEGER,YES
+        3,job ID,INTEGER,YES
+        4,task index,INTEGER,YES
+        5,machine ID,INTEGER,YES
+        6,CPU rate,FLOAT,NO
+        7,canonical memory usage,FLOAT,NO
+        8,assigned memory usage,FLOAT,NO
+        9,unmapped page cache,FLOAT,NO
+        10,total page cache,FLOAT,NO
+        11,maximum memory usage,FLOAT,NO
+        12,disk I/O time,FLOAT,NO
+        13,local disk space usage,FLOAT,NO
+        14,maximum CPU rate,FLOAT,NO
+        15,maximum disk IO time,FLOAT,NO
+        16,cycles per instruction,FLOAT,NO
+        17,memory accesses per instruction,FLOAT,NO
+        18,sample portion,FLOAT,NO
+        19,aggregation type,BOOLEAN,NO
+        20,sampled CPU usage,FLOAT,NO
+
+    """
+    splits = line.split(",")
+    start = splits[0]
+    end = splits[1]
+    machine = splits[4]
+    result = int(end) - int(start)
+    if result < 0:
+        return (machine, 0)
+    else return (machine, result)
+
+distFile = sc.textFile("/home/ks/workspace/data/clusterdata-2011-2/task_usage/part-00000-of-00500.csv")
+task_time = distFile.map(calc_task_time)
+
+""" 
+------------------
+PRICING INPUT DATA
+------------------
+"""
 # values for size: cc2.8xlarge region: us-east
 # 1 year reservation
 c_name = 'cc2.8xlarge'
@@ -24,6 +87,11 @@ h_light = { "upfront" : "3968", "perhour" : "2.24" }
 h_medium = { "upfront" : "9200", "perhour" : "1.38" }
 h_heavy = { "upfront" : "11213", "perhour" : "0.92" }
 
+""" 
+-----------------------------
+INSTANCE ENTITY UTILITY CLASS
+-----------------------------
+"""
 class Entity:
     def __init__(self, name, light, medium, heavy):
         self.name = name
